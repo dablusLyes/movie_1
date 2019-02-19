c395401eea77782528cc30ad12781656e14a95a6
lyes RAMDANE <lyes.dabla@mail.com>
2019-02-19 17:02:31 +0100

    oui

--- inc/header.php
+++ inc/header.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -25,91 +24,6 @@
 						echo '<li><a href="logout.php">Logout</a></li>';
 						echo '<li><p>Bonjour <span>' . $_SESSION['user']['pseudo'] . '</span></p></li>';
 					}
-
-<<<<<<< HEAD
-<form action="search.php" method="post">
-    
-    <label for="pet-select">select a genre</label>
-    
-        <div>
-        <input type="checkbox" id="drama" name="drama"
-                >
-        <label for="drama">drama</label>
-        </div>
-
-        <div>
-        <input type="checkbox" id="fantasy" name="fantasy"
-                >
-        <label for="fantasy">fantasy</label>
-        </div>
-
-        <div>
-        <input type="checkbox" id="romance" name="romance"
-                >
-        <label for="romance">romance</label>
-        </div>
-
-        <div>
-        <input type="checkbox" id="Action" name="Action"
-                >
-        <label for="Action">Action</label>
-        </div>   
-
-        <div>
-        <input type="checkbox" id="Thriller" name="Thriller"
-                >
-        <label for="Thriller">Thriller</label>
-        </div>
-
-        <div>
-        <input type="checkbox" id="Animation" name="Animation"
-                >
-        <label for="Animation">Animation</label>
-        </div>
-
-        <div>
-        <input type="checkbox" id="comedy" name="comedy"
-                >
-        <label for="comedy">comedy</label>
-        </div>
-
-        <div>
-        <input type="checkbox" id="Mystery" name="Mystery"
-                >
-        <label for="Mystery">Mystery</label>
-        </div>
-
-        <div>
-        <input type="checkbox" id="Crime" name="Crime">
-        <label for="Crime">Crime</label>
-        </div>
-
-        <div>
-        <input type="checkbox" id="sci-fi" name="sci-fi"
-                >
-        <label for="sci-fi">sci-fi</label>
-        </div>
-
-        <div>
-        <input type="checkbox" id="Biography" name="Biography"
-                >
-        <label for="Biography">Biography</label>
-        </div>
-
-            <label for="year">Release date between</label>
-    <select name="year" id="year">
-        <?php for ($i = 1900; $i < 2020; $i++) { ?><option value="<?php echo $i; ?>"><?php echo $i; ?></option><?php }  ?>
-    </select>
-
-    <label for="year">and</label>
-    <select name="year2" id="year">
-        <?php for ($i = 1900; $i < 2020; $i++) { ?><option value="<?php echo $i; ?>"><?php echo $i; ?></option><?php }  ?>
-    </select>
-</form>
-        
-    </header>              <!--fin header-->
-     <div id="section">    <!--debut section-->
-=======
 					if (isAdmin()) {
 						echo '<li><a href="admin/index.php">Back office</a></li>';
 					} ?>	
@@ -117,7 +31,6 @@
 			</nav>
 		</header>
      	<section id="section">
->>>>>>> d7da64fd3b576b5d578182d7901566e2fae43bbc
 
 
     
--- index.php
+++ index.php
@@ -8,6 +8,77 @@ $films = getRandomMovies();
 
 include('inc/header.php');
 
+?>
+<form action="search.php" method="post">
+    
+    <label for="genre-select">select a genre</label>
+
+    <input type="checkbox" id="drama" name="genre[]">
+    <label for="drama">drama</label>
+
+    <input type="checkbox" id="fantasy" name="genre[]">
+    <label for="fantasy">fantasy</label>
+
+    <input type="checkbox" id="romance" name="genre[]">
+    <label for="romance">romance</label>
+
+    <input type="checkbox" id="Action" name="genre[]">
+    <label for="Action">Action</label>
+
+    <input type="checkbox" id="Thriller" name="genre[]">
+    <label for="Thriller">Thriller</label>
+    <input type="checkbox" id="Animation" name="genre[]" >
+    <label for="Animation">Animation</label>
+    <br>
+
+    <input type="checkbox" id="comedy" name="genre[]">
+    <label for="comedy">comedy</label>
+
+    <input type="checkbox" id="Mystery" name="genre[]">
+    <label for="Mystery">Mystery</label>
+
+    <input type="checkbox" id="Crime" name="genre[]">
+    <label for="Crime">Crime</label>
+
+    <input type="checkbox" id="sci-fi" name="genre[]">
+    <label for="sci-fi">sci-fi</label>
+
+    <input type="checkbox" id="Biography" name="genre[]">
+    <label for="Biography">Biography</label>
+
+    <label for="year">Release date between</label>
+    <select name="year" id="year">
+    <?php for ($i = 1900; $i < 2020; $i++) { ?><option value="<?php echo $i; ?>"><?php echo $i; ?></option><?php }  ?>
+    </select>
+
+    <label for="year">and</label>
+    <select name="year2" id="year">
+    <?php for ($i = 1900; $i < 2020; $i++) { ?><option value="<?php echo $i; ?>"><?php echo $i; ?></option><?php }  ?>
+    </select>
+</form>
+
+
+
+
+<?php
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 foreach($films as $film){
 ?>
 <ul>
