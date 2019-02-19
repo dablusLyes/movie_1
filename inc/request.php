bb65492d77e509a5f12f8cc75ec105dee62f6b27
lyes RAMDANE <lyes.dabla@mail.com>
2019-02-19 12:11:51 +0100

    modif sur request + le front

--- inc/request.php
+++ inc/request.php
@@ -1,13 +1,14 @@
 <?php
 
 
+
 function getRandomMovies(){
-        global $pdo;
-        $sql = "SELECT * FROM movies_full 
-                ORDER BY RAND() 
-                LIMIT 10 ";
-            $query = $pdo->prepare($sql);
-            $query->execute();
-   $table = $query->fetchALL();
-        return $table;
-    }
+    global $pdo;
+    $sql = "SELECT * FROM movies_full 
+            ORDER BY RAND() 
+            LIMIT 10 ";
+        $query = $pdo->prepare($sql);
+        $query->execute();
+$table = $query->fetchALL();
+    return $table;
+}
--- index.php
+++ index.php
@@ -3,10 +3,21 @@ include('inc/function.php');
 include('inc/pdo.php');
 include('inc/request.php');
 
-debug(getRandomMovies());
-
+$films = getRandomMovies();
 
 
 include('inc/header.php');
 
 
+
+foreach($films as $film){
+?>
+<ul>
+<img src="posters/<?php echo $film['id']; ?>.jpg" alt="<?php echo $film['title'] ?>">
+    <li> <?php echo $film['title'] ?> </li>
+    <li> year of sortie<?php echo $film['year'] ?> </li>
+    <li> rating : <?php echo $film['rating'] ?>/100</li>
+</ul>
+
+<?php 
+}
