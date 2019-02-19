fca48694bab1e7817ba8fe5777614b87f4ff3ee1
Remi Molinero <contact@remimolinero.com>
2019-02-19 12:33:16 +0100

    update login.php

--- login.php
+++ login.php
@@ -1,13 +1,16 @@
-<?php session_start() ?>
-<?php include('inc/request.php'); ?>
-<?php include('inc/pdo.php'); ?>
-<?php
+<?php 
+session_start();
+include('inc/request.php'); 
+include('inc/pdo.php');
+require 'inc/function.php';
+
 
 $errors = [];
 
 if (!empty($_POST['submitted'])) {
   $login = trim(strip_tags($_POST['login']));
   $password = trim(strip_tags($_POST['password']));
+
   if (empty($login) OR empty($password)) {
     $errors['login'] = 'Veuillez renseignez les deux champs pour vous connectez';
   }
@@ -18,47 +21,43 @@ if (!empty($_POST['submitted'])) {
     $query->bindValue(':login',$login,PDO::PARAM_STR);
     $query->execute();
     $user = $query->fetch();
+
     if (!empty($user)) {
-       debug($user);
-       die();
+
       if (password_verify($password,$user['password'])) {
         //   tout est verifié, creation de la session user
         $_SESSION['user'] = array(
+          'id' => $user['id'],
           'email' => $user['email'],
           'pseudo' => $user['pseudo'],
-          'password' => $user['password'],
+          'role' => $user['role'],
+          'ip' => $_SERVER['REMOTE_ADDR']
         );
         header('Location: index.php');
-              die();
-            }else {
-              $errors['login'] = 'Erreur de connexion';
-            }
-          }else {
-            $errors['login'] = 'Erreur de connexion';
-          }
-        }
+        exit;
+      } else {
+        $errors['login'] = 'Erreur de connexion';
       }
+    } else {
+      $errors['login'] = 'Erreur de connexion';
+    }
+  }
+}
 
-
-?>
-<?php include('inc/header.php'); ?>
+ include('inc/header.php'); ?>
 
 <form action="" method="post">
   <label for="login">Votre pseudo ou email *</label>
   <input type="text" id="login" name="login" value="<?php if (!empty($_POST['login'])) { echo $_POST['login']; } ?>" placeholder="Pseudo ou email ici...">
-  <span class="error" style="color:red"><?php if (!empty($errors['login'])) { echo $errors['login']; } ?></span>
+  <span class="error"><?php if (!empty($errors['login'])) { echo $errors['login']; } ?></span>
 
   <label for="password">Votre mot de passe *</label>
   <input type="password" id="password" name="password" value="" placeholder="Votre mot de passe ex: Le mot de passe">
-  <span class="error" style="color:red"><?php if (!empty($errors['password'])) { echo $errors['password']; } ?></span>
+  <span class="error"><?php if (!empty($errors['password'])) { echo $errors['password']; } ?></span>
 
 <input type="submit" name="submitted" value="Connexion">
 </form>
 
 <a href="forgetpassword.php">Mot de passe oublié</a>
-
-<?php
-
-?>
 
 <?php include('inc/footer.php');
