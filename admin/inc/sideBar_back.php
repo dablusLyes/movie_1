4cd8e0c4e211895c00c15d61db68b9dc6d225648
Valentin LAIDET <laidet.val@hotmail.fr>
2019-02-19 15:20:23 +0100

    backoffice

---
+++ admin/inc/sideBar_back.php
@@ -0,0 +1,51 @@
+<div class="left-sidebar-pro">
+        <nav id="sidebar" class="">
+            <div class="sidebar-header">
+                <a href="index.php"><img class="main-logo" src="img/logo/logo.png" alt="" /></a>
+                <strong><img src="img/logo/logosn.png" alt="" /></strong>
+            </div>
+			<div class="nalika-profile">
+				<div class="profile-dtl">
+					<a href="#"><img src="img/notification/4.jpg" alt="" /></a>
+					<h2>Lakian <span class="min-dtn">Das</span></h2>
+				</div>
+			</div>
+            <div class="left-custom-menu-adp-wrap comment-scrollbar">
+                <nav class="sidebar-nav left-sidebar-menu-pro">
+                    <ul class="metismenu" id="menu1">
+                        <li class="active">
+                            <a class="has-arrow" href="index.php">
+								   <i class="icon nalika-home icon-wrap"></i>
+								   <span class="mini-click-non">Dashbord</span>
+								</a>
+                            <ul class="submenu-angle" aria-expanded="true">
+                            </ul>
+                        </li>
+                        <li>
+                            <a class="has-arrow" href="#" aria-expanded="false"><i class="icon nalika-pie-chart icon-wrap"></i> <span class="mini-click-non">Miscellaneous</span></a>
+                            <ul class="submenu-angle" aria-expanded="false">
+                            </ul>
+                        </li>
+                        <li>
+                            <a class="has-arrow" href="#" aria-expanded="false"><i class="icon nalika-bar-chart icon-wrap"></i> <span class="mini-click-non">Charts</span></a>
+                            <ul class="submenu-angle" aria-expanded="false">
+                            </ul>
+                        </li>
+                        <li>
+                            <a class="has-arrow" href="#" aria-expanded="false"><i class="icon nalika-table icon-wrap"></i> <span class="mini-click-non">Data Tables</span></a>
+                            <ul class="submenu-angle" aria-expanded="false">
+                            </ul>
+                        </li>
+                        <li>
+                            <a class="has-arrow" href="#" aria-expanded="false"><i class="icon nalika-forms icon-wrap"></i> <span class="mini-click-non">Forms Elements</span></a>
+                        </li>
+                        <li id="removable">
+                            <a class="has-arrow" href="#" aria-expanded="false"><i class="icon nalika-new-file icon-wrap"></i> <span class="mini-click-non">Pages</span></a>
+                            <ul class="submenu-angle" aria-expanded="false">
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </nav>
+    </div>
---
+++ admin/index.php
@@ -0,0 +1,6 @@
+<?php include('inc/header_back.php'); ?>
+<?php include('inc/sideBar_back.php'); ?>
+<?php include('inc/headerBar_back.php'); ?>
+<?php include('inc/mobileMenu_back.php'); ?>
+
+<?php include('inc/footer_back.php') ?>
