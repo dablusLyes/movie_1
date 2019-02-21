7abc831e4a103061ae318757255ae46bdf9460be
lyes RAMDANE <lyes.dabla@mail.com>
2019-02-21 14:07:58 +0100

    oui

--- index.php
+++ index.php
@@ -12,37 +12,38 @@ include('inc/header.php'); ?>
     
     <label for="genre-select">select a genre</label>
 
-    <input type="checkbox" id="drama" name="genre[]">
+    <input type="checkbox" value="drama" name="genre[]">
     <label for="drama">drama</label>
 
-    <input type="checkbox" id="fantasy" name="genre[]">
+    <input type="checkbox" value="fantasy" name="genre[]">
     <label for="fantasy">fantasy</label>
 
-    <input type="checkbox" id="romance" name="genre[]">
+    <input type="checkbox" value="romance" name="genre[]">
     <label for="romance">romance</label>
 
-    <input type="checkbox" id="Action" name="genre[]">
+    <input type="checkbox" value="Action" name="genre[]">
     <label for="Action">Action</label>
 
-    <input type="checkbox" id="Thriller" name="genre[]">
+    <input type="checkbox" value="Thriller" name="genre[]">
     <label for="Thriller">Thriller</label>
-    <input type="checkbox" id="Animation" name="genre[]" >
+
+    <input type="checkbox" value="Animation" name="genre[]" >
     <label for="Animation">Animation</label>
     <br>
 
-    <input type="checkbox" id="comedy" name="genre[]">
+    <input type="checkbox" value="comedy" name="genre[]">
     <label for="comedy">comedy</label>
 
-    <input type="checkbox" id="Mystery" name="genre[]">
+    <input type="checkbox" value="Mystery" name="genre[]">
     <label for="Mystery">Mystery</label>
 
-    <input type="checkbox" id="Crime" name="genre[]">
+    <input type="checkbox" value="Crime" name="genre[]">
     <label for="Crime">Crime</label>
 
-    <input type="checkbox" id="sci-fi" name="genre[]">
+    <input type="checkbox" value="sci-fi" name="genre[]">
     <label for="sci-fi">sci-fi</label>
 
-    <input type="checkbox" id="Biography" name="genre[]">
+    <input type="checkbox" value="Biography" name="genre[]">
     <label for="Biography">Biography</label>
 
     <label for="year">Release date between</label>
@@ -50,10 +51,11 @@ include('inc/header.php'); ?>
     <?php for ($i = 1900; $i < 2020; $i++) { ?><option value="<?php echo $i; ?>"><?php echo $i; ?></option><?php }  ?>
     </select>
 
-    <label for="year">and</label>
+    <label for="year2">and</label>
     <select name="year2" id="year">
     <?php for ($i = 1900; $i < 2020; $i++) { ?><option value="<?php echo $i; ?>"><?php echo $i; ?></option><?php }  ?>
     </select>
+    <input type="submit" name="submit" value="search">
 </form>
 
 <?php foreach($films as $film){ ?>
---
+++ search.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+include('inc/function.php');
+include('inc/pdo.php');
+include('inc/request.php');
+
+
+
+$x = $_POST['year'];
+$y = $_POST['year2'];
+$sql = "SELECT * FROM movies_full WHERE 1 = 1";
+$z = "AND";
+if(!empty($_POST['submit'])){
+    if(!empty($_POST['genre'])){
+        $check = $_POST['genre'];
+        //debug($check);
+        foreach ($check as $b) {
+            $sql.= $z ." genres LIKE '%".$b."%' ";
+            //echo $sql;   
+            $z = 'OR';        
+         }
+         $sql.= " AND year BETWEEN $x AND $y";
+         //echo $sql;
+        $query = $pdo->prepare($sql);
+        $query->execute();
+        $results = $query->fetchALL();
+        //debug($results);
+
+
+    }else{ header('Location: index.php');}
+}else{header('Location: index.php');}
+
+include('inc/header.php');
+foreach ($results as $result) {
+   ?>
+
+    <a href="details.php?slug=<?php echo $result['slug']; ?>"> <img src="<?php echo poster($result); ?>" alt="<?php echo $film['title'] ?>"></a>
+    <p> <?php echo $result['title'] ?> </p>
+    <p> year of sortie<?php echo $result['year'] ?> </p>
+    <p> rating : <?php echo $result['rating'] ?>/100</p>
+
+
+   <?php
+}
